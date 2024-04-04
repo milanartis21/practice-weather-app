7c618ba97697d395dca9aaa4425e838bc91c37a7
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-04-04 15:46:11 -0600

    just added bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">
-    Weather App
+  return (
+   
+  <div className="App">
+  <div className="container">
+   <h1> Weather App</h1>
     <footer>
       Coded by Milan Artis
    
     <a href= " https://github.com/milanartis21/practice-weather-app"
     target="_blank">
-       Open sourced on GitHub</a>
+        Open sourced on GitHub</a>
  </footer>
  </div>
-  
-}
+ </div>
+  ); 
+};
 
 
