b1fc4cc751faf2af3bcbfc7eea1b79ded8a93528
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-04-03 20:19:15 -0600

    put in the github link to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App">
+    Weather App
+    <footer>
+      Coded by Milan Artis
+   
+    <a href= " https://github.com/milanartis21/practice-weather-app"
+    target="_blank">
+       Open sourced on GitHub</a>
+ </footer>
+ </div>
+  
 }
 
-export default App;
+
